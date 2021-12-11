1a4ff73a895cd55d2588aaa164c25ce94fa91b3f
Robert <houtong_an@163.com>
2021-12-11 12:59:32 +0800

    camelot read pdf table into csv

--- Part_2/ch13_pdfWord/Camelot/camelot_demo.py
+++ Part_2/ch13_pdfWord/Camelot/camelot_demo.py
@@ -8,14 +8,18 @@ def pdfToCsv():
     '''
     current_path = os.path.dirname(sys.argv[0])
     fname = os.path.join(current_path, 'pdf', 'foo.pdf')
+    #fname = os.path.join(current_path, '2020年人口普查.pdf')
+
     tables = camelot.read_pdf(fname)
     print('tables:\n',tables)
+    
     # json, excel, html, markdown, sqlite
     tables.export(os.path.join(current_path, 'foo_all.csv'), f='csv', compress=True)
     print('tables[0]:\n',tables[0])
     print('tables[0].parsing_report:\n',tables[0].parsing_report)
+
     # to_json, to_excel, to_html, to_markdown, to_sqlite
-    tables[0].to_csv(os.path.join(current_path, 'foo_0.csv'))
+    tables[0].to_csv(os.path.join(current_path, 'foo_all.csv'))
     # get a pandas DataFrame!
     print('tables[0].df:\n',tables[0].df)
 
